82a13d0d64eda26b15f2d25ff73db8b54c0f63bd
Danil <liceistv1@yandex.ru>
2020-12-11 14:28:15 +0300

    уровни +

--- src/Sidebar.js
+++ src/Sidebar.js
@@ -11,14 +11,13 @@ import { Divider, FormControl, FormGroup, InputLabel, ListSubheader, MenuItem, S
 
 const gyms = [
   {
-    type: 'Strong',
+    id: 1,
+    type: 'Power',
+    items: [
+      { name: "Йога", isChecked: true }
+    ]
   },
 ];
-
-
-
-
-
 
 const useStyles = makeStyles((theme) => ({
   contaner: {
@@ -28,61 +27,77 @@ const useStyles = makeStyles((theme) => ({
     minWidth: 220,
   },
 }));
+function SetCheckedGym() {
+
+}
 
 
 export default function Sidebar() {
   const classes = useStyles();
-
-  const [state, setState] = React.useState({
-    checkedPower: true,
-    checkedStratch: true,
-    checkedPool: true,
+  const [stateall, setState2] = React.useState({
+    allChecked: true,
   });
+  const [state2, setState] = React.useState({
+    checkedLittle: true,
+    checkedMiddle: true,
+    checkedBig: true,
+  });
+  const onAll = () => {
+    setState2({ ...stateall, allChecked: true });
+  };
+  const offAll = () => {
+    setState2({ ...stateall, allChecked: false });
+  };
 
 
   const handleChange = (event) => {
-    setState({ ...state, [event.target.name]: event.target.checked });
+    setState({ ...state2, [event.target.name]: event.target.checked });
   };
 
   return (
     <Grid container direction={"row"} justify={"space-around"} alignItems={"center"} className={classes.contaner}>
-      <Grid item container xs={4}>
+      <Grid item container justify={"space-around"} xs={4}>
+        <Grid item container justify={'center'} xs={12}>
+          <Typography variant="h5">
+            Уровень нагрузки:
+          </Typography>
+        </Grid>
         <Grid item xs={4}>
           <FormControlLabel
-            control={<Checkbox checked={state.checkedPower} onChange={handleChange} name="checkedPower" />}
-            label="Силовые"
+            control={<Checkbox checked={state2.checkedLittle} onChange={handleChange} name="checkedLittle" />}
+            label="Легкий"
           />
         </Grid>
         <Grid item xs={4}>
           <FormControlLabel
-            control={<Checkbox checked={state.checkedStratch} onChange={handleChange} name="checkedStratch" />}
-            label="Растяжка"
+            control={<Checkbox checked={state2.checkedMiddle} onChange={handleChange} name="checkedMiddle" />}
+            label="Средний"
           />
         </Grid>
         <Grid item xs={4}>
           <FormControlLabel
-            control={<Checkbox checked={state.checkedPool} onChange={handleChange} name="checkedPool" />}
-            label="Бассейн"
+            control={<Checkbox checked={state2.checkedBig} onChange={handleChange} name="checkedBig" />}
+            label="Большой"
           />
         </Grid>
       </Grid>
 
       <Grid item xs={4}>
-      <FormControl className={classes.formControl}>
-        <InputLabel htmlFor="grouped-select">Тренеровки</InputLabel>
-        <Select defaultValue="" id="grouped-select">
-          <MenuItem value="">
-            <em>Все</em>
-          </MenuItem>
-          <ListSubheader>Силовые</ListSubheader>
-          <MenuItem value={1}>Option 1</MenuItem>
-          <MenuItem value={2}>Option 2</MenuItem>
-          <ListSubheader>Растяжка</ListSubheader>
-          <ListSubheader>Бассейн</ListSubheader>
-          <MenuItem value={3}>Option 3</MenuItem>
-          <MenuItem value={4}>Option 4</MenuItem>
-        </Select>
-      </FormControl>
+        <FormControl className={classes.formControl}>
+          <InputLabel htmlFor="grouped-select">Тренеровки</InputLabel>
+          <Select defaultValue="" id="grouped-select">
+            <MenuItem value="" onClick={onAll}>
+              <em>Все</em>
+            </MenuItem>
+            <ListSubheader>Силовые</ListSubheader>
+            <MenuItem value={1}>Option 1</MenuItem>
+            <MenuItem value={2}>Option 2</MenuItem>
+            <ListSubheader>Растяжка</ListSubheader>
+            <ListSubheader>Бассейн</ListSubheader>
+            <MenuItem value={3}>Option 3</MenuItem>
+            <MenuItem value={4}>Option 4</MenuItem>
+          </Select>
+        </FormControl>
       </Grid>
       {/* <Grid item xs={4}>
        
