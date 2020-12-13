f5b8537030decf8ba76914fa6a31b3e1f70831d6
Danil <liceistv1@yandex.ru>
2020-12-13 17:01:55 +0300

    so so

--- src/CustomizedDialog.js
+++ src/CustomizedDialog.js
@@ -1,44 +1,7 @@
 import React from 'react';
-import { AppBar, Avatar, Grid, Tab, Tabs, Typography } from '@material-ui/core';
+import { Avatar, Grid,Typography } from '@material-ui/core';
 import { getCoach } from './data';
-import PropTypes from 'prop-types';
-import SwipeableViews from 'react-swipeable-views';
-import { makeStyles, useTheme } from '@material-ui/core/styles';
-import Box from '@material-ui/core/Box';
-
-
-function TabPanel(props) {
-    const { children, value, index, ...other } = props;
-
-    return (
-        <div
-            role="tabpanel"
-            hidden={value !== index}
-            id={`full-width-tabpanel-${index}`}
-            aria-labelledby={`full-width-tab-${index}`}
-            {...other}
-        >
-            {value === index && (
-                <Box p={3}>
-                    <Typography>{children}</Typography>
-                </Box>
-            )}
-        </div>
-    );
-}
-
-TabPanel.propTypes = {
-    children: PropTypes.node,
-    index: PropTypes.any.isRequired,
-    value: PropTypes.any.isRequired,
-};
-
-function a11yProps(index) {
-    return {
-        id: `full-width-tab-${index}`,
-        'aria-controls': `full-width-tabpanel-${index}`,
-    };
-}
+import { makeStyles } from '@material-ui/core/styles';
 
 const TheStyles = makeStyles((theme) => ({
     avatar: {
@@ -47,10 +10,6 @@ const TheStyles = makeStyles((theme) => ({
     },
     cont: {
         padding: 10,
-    },
-    root: {
-        backgroundColor: theme.palette.background.paper,
-        width: 500,
     },
 }));
 
@@ -61,20 +20,8 @@ export default function CustomizedDialog(props) {
     const coach = getCoach(coach_id);
     const ava = (typeof coach['avatar'] !== "undefined") ? coach.avatar : "https://www.hausarzt-albers.de/wp-content/uploads/2016/08/no-avatar-m.jpg";
 
-    const theme = useTheme();
-    const [value, setValue] = React.useState(0);
-
-    const handleChange = (event, newValue) => {
-        setValue(newValue);
-    };
-
-    const handleChangeIndex = (index) => {
-        setValue(index);
-    };
-
     return (
-        {/* <Grid contaner direction='column' spacing={10}> */}
-            <Grid container justify='flex-start' alignItems='center'>
+            <Grid item xs={12} container justify='flex-start' alignItems='center'>
                 <Grid item xs={4} container className={classes.cont} >
                     <Avatar big src={ava} className={classes.avatar} />
                 </Grid>
@@ -91,37 +38,5 @@ export default function CustomizedDialog(props) {
                     </Typography>
                 </Grid>
             </Grid>
-            {/* <Grid item contaner>
-                <AppBar position="static" color="default">
-                    <Tabs
-                        value={value}
-                        onChange={handleChange}
-                        indicatorColor="primary"
-                        textColor="primary"
-                        variant="fullWidth"
-                        aria-label="full width tabs example"
-                    >
-                        <Tab label="Item One" {...a11yProps(0)} />
-                        <Tab label="Item Two" {...a11yProps(1)} />
-                        <Tab label="Item Three" {...a11yProps(2)} />
-                    </Tabs>
-                </AppBar>
-                <SwipeableViews
-                    axis={theme.direction === 'rtl' ? 'x-reverse' : 'x'}
-                    index={value}
-                    onChangeIndex={handleChangeIndex}
-                >
-                    <TabPanel value={value} index={0} dir={theme.direction}>
-                        Item One
-        </TabPanel>
-                    <TabPanel value={value} index={1} dir={theme.direction}>
-                        Item Two
-        </TabPanel>
-                    <TabPanel value={value} index={2} dir={theme.direction}>
-                        Item Three
-        </TabPanel>
-                </SwipeableViews>
-            </Grid> */}
-       /*  </Grid> */
     );
 }
